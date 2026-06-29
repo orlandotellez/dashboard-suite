3d255c92bd6939990420789c0955b25925d13f70
orlandotellez <espadachinkinno@gmail.com>
2026-06-28 20:04:06 -0600

    feat: agregar conexion a la base de datos y tambien agregar constants y logger

--- templates/01-pos-system/backend-rust/src/main.rs
+++ templates/01-pos-system/backend-rust/src/main.rs
@@ -1,20 +1,34 @@
+pub mod database;
+pub mod shared;
+
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use axum::{Router, routing::get};
+use dotenvy::dotenv;
 use tokio::net::TcpListener;
+
+use crate::database::connection::create_pool;
 
 const HOST: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
 const PORT: u16 = 4000;
 
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    shared::config::logger::init();
+
     let addr: SocketAddr = SocketAddr::new(HOST, PORT);
 
-    let router: Router = Router::new().route("/", get(|| async { "Hola mundo" }));
+    let db = create_pool().await.expect("Error connect database");
+
+    let router: Router = Router::new()
+        .route("/", get(|| async { "Hola mundo" }))
+        .with_state(db);
 
     let listener: TcpListener = TcpListener::bind(&addr).await.unwrap();
 
-    println!("Servidor iniciado en http://{}", &addr);
+    tracing::info!("Server listen on http://{}", &addr);
 
     axum::serve(listener, router).await.unwrap()
 }
