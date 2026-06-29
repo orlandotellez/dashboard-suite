9219bdb4489ed728651135a0d74b319476b5da64
orlandotellez <espadachinkinno@gmail.com>
2026-06-28 19:43:30 -0600

    feat: iniciar servidor de axum con rust

--- templates/01-pos-system/backend-rust/src/main.rs
+++ templates/01-pos-system/backend-rust/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
+use axum::{Router, routing::get};
+use tokio::net::TcpListener;
+
+const HOST: IpAddr = IpAddr::V4(Ipv4Addr::UNSPECIFIED);
+const PORT: u16 = 4000;
+
+#[tokio::main]
+async fn main() {
+    let addr: SocketAddr = SocketAddr::new(HOST, PORT);
+
+    let router: Router = Router::new().route("/", get(|| async { "Hola mundo" }));
+
+    let listener: TcpListener = TcpListener::bind(&addr).await.unwrap();
+
+    println!("Servidor iniciado en http://{}", &addr);
+
+    axum::serve(listener, router).await.unwrap()
 }
